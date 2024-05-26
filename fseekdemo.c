85028d6430a6bfde87e25009612c75fafcddfcd7
Angelo Rossi <angelo.rossi.homelab@gmail.com>
2024-05-26 11:37:27 +0200

    Added factorial.c and fseekdemo.c source files.

--- fseekdemo.c
+++ fseekdemo.c
@@ -15,3 +15,61 @@ tRecord records[ 5 ] = {
   { 4, "user4" },
   { 5, "user5" }
 };
+
+/*
+ * Function prototypes.
+ */
+
+int putRecord(FILE *, int, tRecord *);
+int main(int, char *[]);
+
+/*
+  The main function.
+*/
+
+int main(int argc, char *argv[])
+{
+  int i;
+  FILE *fp;
+  tRecord rec;
+
+  /*
+   * Open the data file for writing.
+   */
+  if((fp = fopen("datafile.dat", "w")) == NULL) {
+    perror("Could not open datafile.dat for writing.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  /*
+   * For each user, going backwards...
+   */
+  for(i = 4; i >= 0; i--) {
+    /*
+     * Output the record.  Notice we pass the address
+     * of the structure.
+     */
+    putRecord(fp, i, &records[ i ]);
+  }
+  fclose(fp);
+  exit(EXIT_SUCCESS);
+}
+
+int putRecord(FILE *fp, int i, tRecord *r)
+{
+  int ret = EXIT_FAILURE;
+  /*
+   * Seek to the i-th position from the beginning
+   * of the file.
+   */
+  if(fp) {
+    if(fseek(fp, (long) (i * sizeof(tRecord)), SEEK_SET) == 0)
+      /*
+       * Write the record.  We want to write  one
+       * object the size of a record structure.
+       */
+      if(fwrite((char *) r, sizeof(tRecord), 1, fp) == 1)
+	ret = EXIT_SUCCESS;
+  }
+  return ret;
+}
