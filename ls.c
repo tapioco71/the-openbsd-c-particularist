1304a770fde7c82afd994e26e7f055e4552b8b9f
Angelo Rossi <angelo.rossi.homelab@gmail.com>
2024-05-28 10:43:46 +0200

    ls.c edited #7.

--- ls.c
+++ ls.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <time.h>
 #include <sys/types.h>
 #include <sys/dir.h>
 #include <sys/stat.h>
@@ -50,7 +51,7 @@ int main(int argc, char *argv[])
     /*
      * Process arguments.
      */
-    while((ch = getopt(argc, argv, "asl:")) != -1) {
+    while((ch = getopt(argc, argv, "als")) != -1) {
       switch(ch) {
       case 'a':
 	flags |= 0x01;
@@ -141,23 +142,100 @@ void printout(char *dir, char *name, uint8_t flags)
    * we have a legal path.
    */
   snprintf(newname, S_BLKSIZE, "%s/%s", dir, name);
-  /*
-   * At this point we know the file exists,
-   * so this won't fail.
-   */
-  stat(newname, &st_buf);
-  if((flags & 0x04) != 0)
+  if((name[ 0 ] != '.') || ((flags & 0x01) != 0)) {
     /*
-     * print size in kbytes.
+     * At this point we know the file exists,
+     * so this won't fail.
      */
-    printf("%5d ", (st_buf.st_size + S_BLKSIZE - 1) / S_BLKSIZE);
+    stat(newname, &st_buf);
+    if((flags & 0x04) != 0) {
+      /*
+       * print size in kbytes.
+       */
+      if((flags & 02) != 0)
+	printf("%5d ", (st_buf.st_size + S_BLKSIZE - 1) / S_BLKSIZE);
+      /*
+       * Get the file type.  For convenience (and to
+       * make this example universal), we ignore the
+       * other types which are version-dependent.
+       */
+      switch(st_buf.st_mode & S_IFMT) {
+      case S_IFREG:
+	putchar('-');
+	break;
 
+      case S_IFDIR:
+	putchar('d');
+	break;
 
+      case S_IFCHR:
+	putchar('c');
+	break;
+
+      case S_IFBLK:
+	putchar('b');
+	break;
+
+      default:
+	putchar('?');
+	break;
+      }
+      /*
+       * Get each of the three groups of permissions
+       * (owner, group, other).  Since they're just
+       * bits, we can count in binary and use this
+       * as subscript (see the modes array, above).
+       */
+      *perms = '\0';
+      for(i = 2; i >= 0; i--) {
+	/*
+	 * Since we're subscripting, we don't
+	 * read the constants.  Just get a
+	 * value between 0 and 7.
+	 */
+	j = (st_buf.st_mode >> (i * 3)) & 0x07;
+	/*
+	 * Get the perm bits.
+	 */
+	strcat(perms, modes[ j ]);
+      }
+      /*
+       * Handle special bits which replace the 'x'
+       * in places.
+       */
+      if((st_buf.st_mode & S_ISUID) != 0)
+	perms[ 2 ] = 's';
+      if((st_buf.st_mode & S_ISGID) != 0)
+	perms[ 5 ] = 's';
+      if((st_buf.st_mode & S_ISVTX) != 0)
+	perms[ 8 ] = 't';
+      /*
+       * Print permissions, number of links,
+       * user and group ids.
+       */
+      printf("%s%3d %5d/%-5d ", perms, st_buf.st_nlink, st_buf.st_uid, st_buf.st_gid);
+      /*
+       * Print the size of the file in bytes.
+       * and the last modification time.  The
+       * ctime routine converts a time to ASCII;
+       * it is described in Chapter 7, Telling
+       * Time and Timing Things.
+       */
+      if((flags & 0x02) != 0)
+	printf("%7d ", st_buf.st_size);
+      printf("%.12s ", ctime(&st_buf.st_mtime) + 4);
+      /*
+       * Finally, print the file name.
+       */
+    }
+    printf("%s", name);
+    putchar('\n');
+  }
 }
 
 void usage(void)
 {
-  printf("Usage: ls [-asl] dir\n");
+  printf("Usage: ls [-a] [-s] [-l] dir\n");
 }
 
 /* End of file listfiles1.c */
