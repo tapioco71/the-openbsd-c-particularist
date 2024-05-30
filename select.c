e9f9d4dde530a7373c4f4cddb8c1bcc2125bd046
Angelo Rossi <angelo.rossi.homelab@gmail.com>
2024-05-30 10:55:26 +0200

    Added select.c file.

---
+++ select.c
@@ -0,0 +1,29 @@
+/* -*- mode: c-mode; -*- */
+/*
+ * select program.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+
+/*
+ * Functions prototypes.
+ */
+int main(int, char *[]);
+
+/*
+ * The main function.
+ */
+int main(int argc, char *argv[])
+{
+  int n, nfds;
+  char buf[ 32 ];
+
+}
