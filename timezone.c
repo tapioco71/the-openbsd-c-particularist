81db8fad14fb2ddf833be9131da65ed5d83bf6f5
Angelo Rossi <angelo.rossi.homelab@gmail.com>
2024-05-31 18:37:55 +0200

    timezone.c edited #5.

--- timezone.c
+++ timezone.c
@@ -14,21 +14,7 @@
 /*
  * Functions prototypes.
  */
-char *dst(int);
 int main(int, char *[]);
-/*
- * Global variables.
- */
-char *dstzones[ DST_CAN + 2 ] = {
-  "None",
-  "USA",
-  "Australia",
-  "Western Europe",
-  "Middle Europe",
-  "Eastern Europe",
-  "Canada",
-  "Unknown"
-};
 
 /*
  * The main function.
@@ -36,23 +22,28 @@ char *dstzones[ DST_CAN + 2 ] = {
 int main(int argc, char *argv[])
 {
   long int ret = EXIT_FAILURE;
-  struct timezone tz;
+  time_t now;
+  struct tm *tm_val;
   /*
    * get-time-of-the-day.
    */
-  if((tz = timezone()) != NULL) {
-    printf("minutes west of Greenwich: %d, ", tz.tz_minuteswest);
-    printf("type of DST correction: %s\n", dst(tz.tz_dsttime));
-    ret = EXIT_SUCCESS;
+  if(gettimeofday(&now, NULL) >= 0) {
+    if((tm_val = localtime(&now)) != NULL) {
+      printf("seconds: %d, ", tm.tm_sec);
+      printf("minutes: %d, ", tm.tm_min);
+      printf("hours: %d, ", tm.tm_hour);
+      printf("day of month: %d, ", tm.tm_mday);
+      printf("month: %d, ", tm.tm_mon);
+      printf("year: %d, ", tm.tm_year);
+      printf("weekday: %d, ", tm.tm_wday);
+      printf("day of year: %d\n", tm.tm_yday);
+      printf("summer time in effect? %d\n", tm.tm_isdst);
+      printf("offset from UTC in seconds: %d\n", tm.tm_gmtoff);
+      printf("timezone name: %s\n", tm.tm_zone);
+      ret = EXIT_SUCCESS;
+    } else
+      perror("Could not get local time");
   } else
-    perror("Could not get timezone");
+    perror("Could not get time-of-the-day");
   exit(ret);
-}
-
-char *dst(int d)
-{
-  if((d >= DST_NONE) && (d <= DST_CAN))
-    return dstzones[ d ];
-  else
-    return dstzones[ DST_CAN + 1 ];
 }
