570812039ed0e76e5bf83ea14f01da1397d34687
Angelo Rossi <angelo.rossi.homelab@gmail.com>
2024-05-31 21:20:46 +0200

    timezone.c edited #14.

--- timezone.c
+++ timezone.c
@@ -27,21 +27,23 @@ int main(int argc, char *argv[])
   /*
    * get-time-of-the-day.
    */
-  now = gettime();
-  if((tm_val = localtime(&now)) != NULL) {
-    printf("seconds: %d, ", tm_val -> tm_sec);
-    printf("minutes: %d, ", tm_val -> tm_min);
-    printf("hours: %d, ", tm_val -> tm_hour);
-    printf("day of month: %d, ", tm_val -> tm_mday);
-    printf("month: %d, ", tm_val -> tm_mon);
-    printf("year: %d, ", tm_val -> tm_year);
-    printf("weekday: %d, ", tm_val -> tm_wday);
-    printf("day of year: %d\n", tm_val -> tm_yday);
-    printf("summer time in effect? %d\n", tm_val -> tm_isdst);
-    printf("offset from UTC in seconds: %d\n", tm_val -> tm_gmtoff);
-    printf("timezone name: %s\n", tm_val -> tm_zone);
-    ret = EXIT_SUCCESS;
+  if(gettimeofday(&now, NULL) >= 0) {
+    if((tm_val = localtime(&now.tv_sec)) != NULL) {
+      printf("seconds: %d, ", tm_val -> tm_sec);
+      printf("minutes: %d, ", tm_val -> tm_min);
+      printf("hours: %d, ", tm_val -> tm_hour);
+      printf("day of month: %d, ", tm_val -> tm_mday);
+      printf("month: %d, ", tm_val -> tm_mon);
+      printf("year: %d, ", tm_val -> tm_year);
+      printf("weekday: %d, ", tm_val -> tm_wday);
+      printf("day of year: %d\n", tm_val -> tm_yday);
+      printf("summer time in effect? %d\n", tm_val -> tm_isdst);
+      printf("offset from UTC in seconds: %d\n", tm_val -> tm_gmtoff);
+      printf("timezone name: %s\n", tm_val -> tm_zone);
+      ret = EXIT_SUCCESS;
+    } else
+      perror("Could not get local time");
   } else
-    perror("Could not get local time");
+    perror("Could not get time-of-the-day");
   exit(ret);
 }
