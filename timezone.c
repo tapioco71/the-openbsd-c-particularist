b47dd6acd93cd2e59cabacdb09b813183eb6c6df
Angelo Rossi <angelo.rossi.homelab@gmail.com>
2024-05-31 17:08:31 +0200

    timezone.c edited #4.

--- timezone.c
+++ timezone.c
@@ -36,15 +36,11 @@ char *dstzones[ DST_CAN + 2 ] = {
 int main(int argc, char *argv[])
 {
   long int ret = EXIT_FAILURE;
-  struct timeval now;
   struct timezone tz;
   /*
    * get-time-of-the-day.
    */
   if((tz = timezone()) != NULL) {
-    printf("time in seconds: %ld, ", now.tv_sec);
-    printf("time in microseconds: %ld\n", now.tv_usec);
-    printf("date: %s\n", ctime(&now.tv_sec));
     printf("minutes west of Greenwich: %d, ", tz.tz_minuteswest);
     printf("type of DST correction: %s\n", dst(tz.tz_dsttime));
     ret = EXIT_SUCCESS;
