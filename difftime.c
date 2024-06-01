bcdcb74dc4b024af4504b826f76a9103b4fad2d3
Angelo Rossi <angelo.rossi.homelab@gmail.com>
2024-06-01 10:51:32 +0200

    file difftime.c edited #24.

--- difftime.c
+++ difftime.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
   if(argc == 2) {
     if((fd_wtmp = open(_PATH_WTMP, O_RDONLY)) >= 0) {
       while(read(fd_wtmp, (void *) &tmp_record, sizeof(struct utmp)) > 0) {
-	if(strncmp((const char *) argv[ 1 ], (const char *) tmp_record.ut_name, UT_NAMESIZE) != 0) {
+	if(strncmp((const char *) argv[ 1 ], (const char *) tmp_record.ut_name, UT_NAMESIZE) == 0) {
 	  memcpy((void *) &login_record, (void *) &tmp_record, sizeof(struct utmp));
 	  printf("Found login name: %s\n", argv[ 1 ]);
 	}
