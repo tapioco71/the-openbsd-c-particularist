da081135f290f868ebdebd8960d92c68b9af9107
Angelo Rossi <angelo.rossi.homelab@gmail.com>
2024-06-01 09:35:20 +0200

    file difftime.c edited #7.

--- difftime.c
+++ difftime.c
@@ -23,40 +23,26 @@ int main(int, char *[]);
  */
 int main(int argc, char *argv[])
 {
-  int fd_utmp, fd_wtmp;
+  int fd_wtmp;
   long int ret = EXIT_FAILURE;
-  struct utmp login_record, logout_record;
+  struct utmp record;
 
   /*
    * Open the /va/run/utmp file.
    */
   if(argc == 2) {
-    if((fd_utmp = open(_PATH_UTMP, O_RDONLY)) >= 0) {
-      if((fd_wtmp = open(_PATH_WTMP, O_RDONLY)) >= 0) {
-	while(read(fd_utmp, (void *) &login_record, sizeof(struct utmp)) > 0) {
-	  if(login_record.ut_name[ 0 ] != '\0') {
-	    if(strncmp(login_record.ut_name, (const char *) argv[ 1 ], UT_NAMESIZE) == 0)
-	      break;
-	  }
+    if((fd_wtmp = open(_PATH_WTMP, O_RDONLY)) >= 0) {
+      while(read(fd_wtmp, (void *) &record, sizeof(struct utmp)) > 0) {
+	if(record.ut_name[ 0 ] != '\0') {
+	  if(strncmp(record.ut_name, (const char *) argv[ 1 ], UT_NAMESIZE) == 0)
+	    printf("%s : %s\n", record.ut_name, ctime(&record.ut_time))
 	}
-	while(read(fd_wtmp, (void *) &logout_record, sizeof(struct utmp)) > 0) {
-	  if(logout_record.ut_name[ 0 ] != '\0') {
-	    if(strncmp(logout_record.ut_name, (const char *) argv[ 1 ], UT_NAMESIZE) == 0)
-	      break;
-	  }
-	}
-	if((login_record.ut_name[ 0 ] != '\0') && (logout_record.ut_name[ 0 ] != '\0')) {
-	  printf("%s: time connected %lf seconds.\n",			\
-		 *argv[ 1 ],						\
-		 difftime(login_record.ut_time, login_record.ut_time));
-	  ret = EXIT_SUCCESS;
-	}
-	close(fd_wtmp);
-      } else
-	perror("Could not open /var/run/wtmp");
-      close(fd_utmp);
+      }
+      ret = EXIT_SUCCESS;
+      close(fd_wtmp);
     } else
-      perror("Could no open /var/run/utmp");
+      perror("Could not open /var/run/wtmp");
+    close(fd_utmp);
   } else
     fprintf(stderr, "Usage: difftime name\n");
   exit(ret);
