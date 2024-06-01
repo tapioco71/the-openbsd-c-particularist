e5a0b232a2b4ac50468c9a98fc0aec3950e28f17
Angelo Rossi <angelo.rossi.homelab@gmail.com>
2024-06-01 10:57:24 +0200

    file difftime.c edited #27.

--- difftime.c
+++ difftime.c
@@ -26,6 +26,7 @@ int main(int argc, char *argv[])
   int fd_wtmp;
   long int ret = EXIT_FAILURE;
   double d;
+  off_t lp;
   struct utmp tmp_record, login_record, logout_record;
 
   /*
@@ -36,8 +37,9 @@ int main(int argc, char *argv[])
       while(read(fd_wtmp, (void *) &tmp_record, sizeof(struct utmp)) > 0) {
 	if(tmp_record.ut_name[ 0 ] != '\0') {
 	  if(strncmp((const char *) argv[ 1 ], (const char *) tmp_record.ut_name, UT_NAMESIZE) == 0) {
+	    lp = lseek(fd_wtmp, 0, SEEK_CURR);
 	    memcpy((void *) &login_record, (void *) &tmp_record, sizeof(struct utmp));
-	    printf("Found login name: %s\n", argv[ 1 ]);
+	    printf("Found login name: %s in position %d\n", argv[ 1 ], lp);
 	  }
 	}
       }
