e651b182060f77f4232d92f856a0f987f9839d83
Angelo Rossi <angelo.rossi.homelab@gmail.com>
2024-06-01 11:17:59 +0200

    file difftime.c edited #36.

--- difftime.c
+++ difftime.c
@@ -50,8 +50,8 @@ int main(int argc, char *argv[])
 	    bzero((void *) &logout_record, sizeof(struct utmp));
 	    while(read(fd_wtmp, (void *) &tmp_record, sizeof(struct utmp)) > 0) {
 	      if(tmp_record.ut_name[ 0 ] == '\0') {
-		printf("Found a logout...\n");
 		if(strncmp((const char *) tmp_record.ut_line, (const char *) login_record.ut_line, UT_LINESIZE) == 0) {
+		  printf("found the corresponding logout entry for %s...\n", argv[ 1 ]);
 		  memcpy((void *) &logout_record, (void *) &tmp_record, sizeof(struct utmp));
 		  break;
 		}
