fde021fd6ec5387b8c007b736c827c64f759f621
Angelo Rossi <angelo.rossi.homelab@gmail.com>
2024-06-02 14:50:11 +0200

    Added cputime.c file.

---
+++ cputime.c
@@ -0,0 +1,51 @@
+/* -*- mode: c-mode; -*- */
+/*
+ * cputime program.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <time.h>
+#include <utmp.h>
+#include <sys/types.h>
+#include <sys/time.h>
+
+/*
+ * Functions prototypes.
+ */
+int main(int, char *[]);
+
+/*
+ * The main function.
+ */
+int main(int argc, char *argv[])
+{
+  int temp, prev, succ;
+  long int ret = EXIT_FAILURE;
+  struct tms before, after;
+  /*
+   * Get current time.
+   */
+  times(&before);
+  /*
+   * some code.
+   */
+  prev = 1;
+  succ = 2;
+  for(i = 1; i < rand(); i++) {
+    temp = prev + succ;
+    prev = succ;
+    succ = temp;
+  }
+  ret = EXIT_SUCCESS;
+  /*
+   * Get time after computation.
+   */
+  times(&after);
+  printf("User time: %ld seconds.\n", after.tm_utime - before.tm_utime);
+  printf("System time: %ld seconds.\n", after.tm_stime - before.tm_stime);
+  exit(ret);
+}
