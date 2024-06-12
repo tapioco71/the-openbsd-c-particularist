49855d411e8ed806cae77573971f2b16023961f0
Angelo Rossi <angelo.rossi.homelab@gmail.com>
2024-06-12 15:15:12 +0200

    File server.c edited #21.

--- server.c
+++ server.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 
   /* setup structures. */
   servaddr.sin_family = AF_INET;
-  servaddr.sin_addr = htonl(INADDR_ANY);
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
   servaddr.sin_port = htons(SERVER_PORT);
   ret = server(&servaddr);
   exit(ret);
