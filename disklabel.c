d2bb6f0ffae9b4cdedd6ae3cd4f16a41b8c494d7
Angelo Rossi <angelo.rossi.homelab@gmail.com>
2024-06-16 18:34:37 +0200

    File disklabel.c edited #7.

--- disklabel.c
+++ disklabel.c
@@ -28,25 +28,19 @@ int main(int argc, char *argv[])
   /* Check arguments. */
   if(argc == 2) {
     if((diskfd = open(argv[ 1 ], O_RDWR)) >= 0) {
-      if(ioctl(diskfd, DIOCGPDINFO) >= 0) {
-	if(lseek(diskfd, 0, SEEK_SET) >= 0) {
-	  if(read(diskfd, &label, sizeof(struct disklabel)) >= 0) {
-	    printf("magic number: 0x%0.8x\n", label.d_magic);
-	    printf("drive type: 0x%0.4x\n", label.d_type);
-	    printf("drive subtype: 0x%0.4x\n", label.d_subtype);
-	    printf("type name: %s\n", label.d_typename);
-	    printf("pack name: %s\n", label.d_packname);
-	    printf("bytes per sector: 0x%0.8x\n", label.d_secsize);
-	    printf("sectors per track: 0x%0.8x\n", label.d_nsectors);
-	    printf("tracks per cylinder: 0x%0.8x\n", label.d_ntracks);
-	    printf("data cylinders per unit: 0x%0.8x\n", label.d_ncylinders);
-	    printf("data sectors per cylinder: 0x%0.8x\n", label.d_secpercyl);
-	    printf("data sectors per unit: 0x%0.8x\n", label.d_secperunit);
-	    ret = EXIT_SUCCESS;
-	  } else
-	    perror("read");
-	} else
-	  perror("lseek");
+      if(ioctl(diskfd, DIOCGPDINFO, &label) >= 0) {
+	printf("magic number: 0x%0.8x\n", label.d_magic);
+	printf("drive type: 0x%0.4x\n", label.d_type);
+	printf("drive subtype: 0x%0.4x\n", label.d_subtype);
+	printf("type name: %s\n", label.d_typename);
+	printf("pack name: %s\n", label.d_packname);
+	printf("bytes per sector: 0x%0.8x\n", label.d_secsize);
+	printf("sectors per track: 0x%0.8x\n", label.d_nsectors);
+	printf("tracks per cylinder: 0x%0.8x\n", label.d_ntracks);
+	printf("data cylinders per unit: 0x%0.8x\n", label.d_ncylinders);
+	printf("data sectors per cylinder: 0x%0.8x\n", label.d_secpercyl);
+	printf("data sectors per unit: 0x%0.8x\n", label.d_secperunit);
+	ret = EXIT_SUCCESS;
       } else
 	perror("ioctl");
       close(diskfd);
