429b547b575fd3e4c0380a2870cb78abd287206f
Angelo Rossi <angelo.rossi.homelab@gmail.com>
2024-06-16 19:37:07 +0200

    File disklabel.c edited #9.

--- disklabel.c
+++ disklabel.c
@@ -27,25 +27,31 @@ int main(int argc, char *argv[])
 
   /* Check arguments. */
   if(argc == 2) {
-    if((diskfd = open(argv[ 1 ], O_RDWR)) >= 0) {
-      if(ioctl(diskfd, DIOCGPDINFO, &label) >= 0) {
-	printf("magic number: 0x%0.8x\n", label.d_magic);
-	printf("drive type: 0x%0.4x\n", label.d_type);
-	printf("drive subtype: 0x%0.4x\n", label.d_subtype);
-	printf("type name: %s\n", label.d_typename);
-	printf("pack name: %s\n", label.d_packname);
-	printf("bytes per sector: 0x%0.8x\n", label.d_secsize);
-	printf("sectors per track: 0x%0.8x\n", label.d_nsectors);
-	printf("tracks per cylinder: 0x%0.8x\n", label.d_ntracks);
-	printf("data cylinders per unit: 0x%0.8x\n", label.d_ncylinders);
-	printf("data sectors per cylinder: 0x%0.8x\n", label.d_secpercyl);
-	printf("data sectors per unit: 0x%0.8x\n", label.d_secperunit);
-	ret = EXIT_SUCCESS;
+    if(pledge("stdio disklabel unveil rpath wpath", NULL) >= 0) {
+      if(unveil(argv[ 1 ], "rw") >= 0) {
+	if((diskfd = open(argv[ 1 ], O_RDWR)) >= 0) {
+	  if(ioctl(diskfd, DIOCGPDINFO, &label) >= 0) {
+	    printf("magic number: 0x%0.8x\n", label.d_magic);
+	    printf("drive type: 0x%0.4x\n", label.d_type);
+	    printf("drive subtype: 0x%0.4x\n", label.d_subtype);
+	    printf("type name: %s\n", label.d_typename);
+	    printf("pack name: %s\n", label.d_packname);
+	    printf("bytes per sector: 0x%0.8x\n", label.d_secsize);
+	    printf("sectors per track: 0x%0.8x\n", label.d_nsectors);
+	    printf("tracks per cylinder: 0x%0.8x\n", label.d_ntracks);
+	    printf("data cylinders per unit: 0x%0.8x\n", label.d_ncylinders);
+	    printf("data sectors per cylinder: 0x%0.8x\n", label.d_secpercyl);
+	    printf("data sectors per unit: 0x%0.8x\n", label.d_secperunit);
+	    ret = EXIT_SUCCESS;
+	  } else
+	    perror("ioctl");
+	  close(diskfd);
+	} else
+	  perror("open");
       } else
-	perror("ioctl");
-      close(diskfd);
+	perror("nveil");
     } else
-      perror("open");
+      perror("pledge");
   } else
     fprintf(stderr, "usage: disklabel <device>\n");
   exit(ret);
