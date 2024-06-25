f11323745dbce31e3f2efc53679e35a88de911e2
Angelo Rossi <angelo.rossi.homelab@gmail.com>
2024-06-25 18:28:57 +0200

    File fft-c.c edited #1.

--- fft-c.c
+++ fft-c.c
@@ -10,14 +10,15 @@
 #include <complex.h>
 #include <string.h>
 
-_Bool dft(double complex x[], size_t count)
+bool dft(double complex x[], size_t count)
 {
   bool ret = false;
   double complex *temp, wn;
   size_t j, k;
 
   if(count > 0) {
-    if((temp = (double complex *) calloc(count, sizeof(double complex))) != NULL) {
+    if((temp = (double complex *)				\
+	calloc(count, sizeof(double complex))) != NULL) {
       for(j = 0; j < count; j++) {
 	for(k = 0; k < count; k++) {
 	  wn = cexp(-2.0 * M_PI * I / (double) count);
