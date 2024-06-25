2598edb95a4d945375d6e89edddd352c5d24f064
Angelo Rossi <angelo.rossi.homelab@gmail.com>
2024-06-25 14:22:07 +0200

    File norm2-c.c edited #1.

--- norm2-c.c
+++ norm2-c.c
@@ -12,7 +12,9 @@ double norm2_(double [], size_t);
 /* Main function. */
 int main(int argc, char *argv[])
 {
-  double values[] = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0 };
+  double values[] = {
+    1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0
+  };
 
   /* Computes the two norm of a vector. */
   printf("2 norm: %lf\n", norm2_(values, 10));
