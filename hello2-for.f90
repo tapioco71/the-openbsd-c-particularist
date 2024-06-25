18c8276b2ec296a1966962ebfe71ea4e27295d40
Angelo Rossi <angelo.rossi.homelab@gmail.com>
2024-06-25 10:05:44 +0200

    File hello2-for.f90 edited #2.

--- hello2-for.f90
+++ hello2-for.f90
@@ -2,13 +2,6 @@
 
 ! hello2-for.f90 file.
 
-module hello2
-
-  use, intrinsic :: iso_c_binding, only: c_int
-  implicit none
-
-end module hello2
-
 subroutine hello(count)
   use, intrinsic :: iso_c_binding, only: c_int
   implicit none
