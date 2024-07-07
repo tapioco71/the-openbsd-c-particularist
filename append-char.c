551461433b93eaef69f832c10190641630bbbecf
Angelo Rossi <angelo.rossi.homelab@gmail.com>
2024-07-07 10:12:13 +0200

    File append-char.c edited #1.

--- append-char.c
+++ append-char.c
@@ -12,35 +12,42 @@ int main(int, char *[]);
 int main(int argc, char *argv[])
 {
   int c;
+  long int ret = EXIT_FAILURE;
   FILE *from, *to;
+
+
   /* Check our arguments. */
-  if(argc != 3) {
-    fprintf(stderr, "Usage: %s from-file to-file\n", *argv);
-    exit(1);
-  }
-  /* Open the from-file for reading. */
-  if((from = fopen(argv[ 1 ], "r")) == NULL) {
-    perror(argv[ 1 ]);
-    exit(1);
-  }
-  /*
-   * Open the to-file for appending.  If to-file does
-   * not exist, fopen will create it.
-   */
-  if((to = fopen(argv[ 2 ], "a")) == NULL) {
-    perror(argv[ 2 ]);
-    exit(1);
-  }
-  /*
-   * Now read characters from from-file until we
-   * hit end-of-file, and put  them onto to-file.
-   */
-  while((c = getc(from)) != EOF)
-    putc(c, to);
-  /* Now close the files. */
-  fclose(to);
-  fclose(from);
-  exit(0);
+  if(argc == 3) {
+
+    /* Open the from-file for reading. */
+    if((from = fopen(argv[ 1 ], "r")) != NULL) {
+
+      /*
+       * Open the to-file for appending.  If to-file does
+       * not exist, fopen will create it.
+       */
+      if((to = fopen(argv[ 2 ], "a")) != NULL) {
+
+	/*
+	 * Now read characters from from-file until we
+	 * hit end-of-file, and put  them onto to-file.
+	 */
+	while((c = getc(from)) != EOF)
+	  putc(c, to);
+
+	/* Now close the output file. */
+	fclose(to);
+	ret = EXIT_SUCCESS;
+      } else
+	perror(argv[ 2 ]);
+
+      /* Now close the input file. */
+      fclose(from);
+    } else
+      perror(argv[ 1 ]);
+  } else
+    fprintf(stderr, "usage: %s from-file to-file\n", *argv);
+  exit(ret);
 }
 
 /* End of append-char.c file. */
